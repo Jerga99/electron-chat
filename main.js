02e4e4dd3c0be1928b16a29ee63f2feeee1549ba
Filip Jerga <jerga99@gmail.com>
2020-09-07 16:05:21 +0200

    Add more features to App

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-
+// Main Process
 const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
+  // Browser Window <- Renderer Process
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
@@ -12,6 +13,19 @@ function createWindow() {
   })
 
   win.loadFile('index.html')
+  win.webContents.openDevTools();
 }
 
 app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+})
