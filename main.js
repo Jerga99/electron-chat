94a15ef0452ca74885d31e5218de44e471c9ad19
Filip Jerga <jerga99@gmail.com>
2020-09-08 10:55:23 +0200

    Main process Notifications

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Main Process
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Notification } = require('electron');
 
 function createWindow() {
   // Browser Window <- Renderer Process
@@ -16,7 +16,12 @@ function createWindow() {
   win.webContents.openDevTools();
 }
 
-app.whenReady().then(createWindow);
+app.whenReady()
+  .then(() => {
+    createWindow();
+    const notification = new Notification({title: 'Hello World', body: 'My test message'});
+    notification.show();
+  });
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
