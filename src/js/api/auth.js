d9c42a5659171f6333e3556752c64332971afaf7
Filip Jerga <jerga99@gmail.com>
2020-09-11 11:10:25 +0200

    Create user profile

--- src/js/api/auth.js
+++ src/js/api/auth.js
@@ -1,12 +1,20 @@
 
 
+
+import db from '../db/firestore';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-export async function register({email, password}) {
+const createUserProfile = userProfile =>
+  db
+    .collection('profiles')
+    .doc(userProfile.uid)
+    .set(userProfile)
+
+export async function register({email, password, username, avatar}) {
   try {
     const { user } = await firebase.auth().createUserWithEmailAndPassword(email, password);
-    return user;
+    await createUserProfile({uid: user.uid, username, email, avatar, joinedChats: []})
   } catch(error) {
     return Promise.reject(error.message);
   }
