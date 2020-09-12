5fc8aa398f09ede414d08bdd3b8f87ee84f61663
Filip Jerga <jerga99@gmail.com>
2020-09-12 15:04:16 +0200

    Provide user to action on login and register

--- src/js/actions/auth.js
+++ src/js/actions/auth.js
@@ -4,7 +4,7 @@ import * as api from '../api/auth';
 export const registerUser = formData => dispatch => {
   dispatch({type: 'AUTH_REGISTER_INIT'});
   return api.register(formData)
-    .then(_ => dispatch({type: 'AUTH_REGISTER_SUCCESS'}))
+    .then(_ => dispatch({type: 'AUTH_REGISTER_SUCCESS', user: {}}))
     .catch(error => dispatch({type: 'AUTH_REGISTER_ERROR', error}))
 }
 
@@ -12,7 +12,7 @@ export const loginUser = formData => dispatch => {
   dispatch({type: 'AUTH_LOGIN_INIT'});
   return api
     .login(formData)
-    .then(_ => dispatch({type: 'AUTH_LOGIN_SUCCESS'}))
+    .then(_ => dispatch({type: 'AUTH_LOGIN_SUCCESS', user: {}}))
     .catch(error => {
       dispatch({type: 'AUTH_LOGIN_ERROR', error})
     })
