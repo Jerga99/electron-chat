c35f2a008a5a00b73355e315c9ea6816f5f96c60
Filip Jerga <jerga99@gmail.com>
2020-09-14 08:55:17 +0200

    Extract users from chat

--- src/js/actions/chats.js
+++ src/js/actions/chats.js
@@ -41,7 +41,14 @@ export const createChat = (formData, userId) => async dispatch => {
 
 export const subscribeToChat = chatId => dispatch =>
   api
-    .subscribeToChat(chatId, (chat) => {
+    .subscribeToChat(chatId, async (chat) => {
+
+      const joinedUsers = await Promise.all(chat.joinedUsers.map(async userRef => {
+        const userSnapshot = await userRef.get();
+        return userSnapshot.data();
+      }))
+
+      debugger
       dispatch({type: 'CHATS_SET_ACTIVE_CHAT', chat})
     })
 
