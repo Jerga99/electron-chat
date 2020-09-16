ea35305a7ab26eab2855118a2fc25f6b1fc293c4
Filip Jerga <jerga99@gmail.com>
2020-09-16 14:48:05 +0200

    Store settings to local torage

--- src/js/store/middlewares/app.js
+++ src/js/store/middlewares/app.js
@@ -11,6 +11,15 @@ export default store => next => action => {
         body: action.isOnline ? 'Online' : 'Offline'
       });
     }
+    case 'SETTINGS_UPDATE': {
+      const { setting, value } = action;
+      const currentSettings = localStorage.getItem('app-settings');
+      const parsedCurrentSettings = currentSettings ? JSON.parse(currentSettings) : {};
+
+      const settings = {...parsedCurrentSettings, [setting]: value}
+      const stringifiedSettings = JSON.stringify(settings);
+      localStorage.setItem('app-settings', stringifiedSettings);
+    }
     case 'AUTH_LOGOUT_SUCCESS': {
       const { messagesSubs } = store.getState().chats;
       if (messagesSubs) {
