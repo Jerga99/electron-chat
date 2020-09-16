30056586917083101822fe45d766d41121c4b8f0
Filip Jerga <jerga99@gmail.com>
2020-09-16 17:38:10 +0200

    Display styling changes

--- src/js/App.js
+++ src/js/App.js
@@ -42,7 +42,12 @@ function AuthRoute({children, ...rest}) {
 }
 
 
-const ContentWrapper = ({children}) => <div className='content-wrapper'>{children}</div>
+const ContentWrapper = ({children}) => {
+  const isDarkTheme  = useSelector(({settings}) => settings.isDarkTheme);
+  return (
+    <div className={`content-wrapper ${isDarkTheme ? 'dark' : 'light'}`}>{children}</div>
+  )
+}
 
 function ChatApp() {
   const dispatch = useDispatch();
--- src/js/components/shared/LoadingView.js
+++ src/js/components/shared/LoadingView.js
@@ -1,17 +1,21 @@
 
 
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Loader from './Loader';
 
 
 export default function LoadingView({message = 'Just one moment please...'}) {
+  const isDarkTheme = useSelector(({settings}) => settings.isDarkTheme);
 
   return (
-    <div className="loading-screen">
-      <div className="loading-view">
-        <div className="loading-view-container">
-          <div className="mb-3">{message}</div>
-          <Loader />
+    <div className={isDarkTheme ? 'dark' : 'light'}>
+      <div className="loading-screen">
+        <div className="loading-view">
+          <div className="loading-view-container">
+            <div className="mb-3">{message}</div>
+            <Loader />
+          </div>
         </div>
       </div>
     </div>
